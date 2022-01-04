81758326574d89872345bfe0b8655ba9fb86d21c
Prajay Tipre <prajaytipre@gmail.com>
2022-01-04 16:56:51 -0500

    commit tree db73e606488cb47622a944e9ed594865d8c3b9ad

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+  
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+
+    @list = [@a, @b, @c]
+  end
+
+  class TriangleError < StandardError
+
+    def message
+      puts "Please check to ensure that all sides are greater than 0 and that any two sides are greater than the third"
+    end
+  end
+
+
+  def kind
+    if (@a > 0 && @b > 0 && @c > 0) && (@a + @b > @c) && (@a + @c > @b) && (@b + @c > @a)
+      if @list.tally.length == 3
+        :scalene
+      elsif @list.tally.length == 2
+        :isosceles
+      else
+        :equilateral
+      end
+    else
+      begin
+        raise TriangleError
+        puts TriangleError.message
+      end
+    end
+  end
+
+
+    
 end
